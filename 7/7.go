45a5165995bc22687374ecd2c2922955b466169b
Jesse Lucas <jesse@forestgiant.com>
2016-08-31 22:30:37 -0400

    Updating isPrime function

--- 7/7.go
+++ 7/7.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // 10001st prime
 // https://projecteuler.net/problem=7
@@ -31,7 +34,7 @@ func isPrime(n int) bool {
 		return false
 	}
 
-	for i := 2; i < n; i++ {
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
 		// no positive divisors other than 1 and itself
 		if n%i == 0 {
 			return false
